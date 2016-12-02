1f89be111cbbc3a963fa80418dfa5f6f0d58ac77
mxy2008 <xinyima9@gmail.com>
2016-12-02 16:07:32 -0500

    directory

--- tests/run.sh
+++ tests/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-
-python ../tsbtreedb/similarity.py
+current_dir=$(pwd)
+shortName=`echo $current_dir | rev | cut -d"/" -f2-  | rev`
+file_dir=$shortName'/''tsbtreedb''/''similarity.py'
+#echo $file_dir
+#python ../tsbtreedb/similarity.py
+python $file_dir
