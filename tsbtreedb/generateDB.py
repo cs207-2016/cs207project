0b66703296b8ed17f5c08e27982b96ce2e971263
arielhv <ariel_herbertvoss@g.harvard.edu>
2016-12-03 19:11:37 -0700

    Reformat pass on tsbtreedb

--- tsbtreedb/generateDB.py
+++ tsbtreedb/generateDB.py
@@ -1,10 +1,11 @@
 import os, sys
+
 curr_dir = os.getcwd().split('/')
 sys.path.append('/'.join(curr_dir[:-1]))
 ts_dir = curr_dir[:-1]
 ts_dir.append('timeseries')
 sys.path.append('/'.join(ts_dir))
-#sys.path.append('/Users/mxy/Downloads/Milestone2-part7')
+# sys.path.append('/Users/mxy/Downloads/Milestone2-part7')
 
 import json
 
@@ -34,7 +35,8 @@ A file called 'vp_origin_idx.dat' is also genenrated when this function is calle
 It saves the index of each vantage point in the set of time series.
 '''
 
-def generateDB(numTS = 1000, numVP = 20):
+
+def generateDB(numTS=1000, numVP=20):
     '''
     Generate some Databases from different timeseries vantage points. Each database
     is generated from each vantage point.
@@ -46,7 +48,7 @@ def generateDB(numTS = 1000, numVP = 20):
     '''
     num_ts = numTS
     num_vp = numVP
-    vantage_point_index_list = np.random.choice(num_ts, num_vp, replace = False)
+    vantage_point_index_list = np.random.choice(num_ts, num_vp, replace=False)
     file_name = 'vp_origin_idx.dat'
     try:
         f = open(file_name, 'w')
@@ -56,24 +58,24 @@ def generateDB(numTS = 1000, numVP = 20):
         print("Unexpected error:", sys.exc_info()[0])
         raise
 
-    for i in range(len(vantage_point_index_list)-1):
+    for i in range(len(vantage_point_index_list) - 1):
         f.write(str(vantage_point_index_list[i]))
         f.write(',')
-    f.write(str(vantage_point_index_list[len(vantage_point_index_list)-1]))
+    f.write(str(vantage_point_index_list[len(vantage_point_index_list) - 1]))
     f.close()
-    vantage_point_list  = []
-    #print (vantage_point_index_list)
+    vantage_point_list = []
+    # print (vantage_point_index_list)
     directory = "tsdb"
     if not os.path.exists(directory):
         os.makedirs(directory)
 
     for i in range(num_vp):
         file_name = 'tsfiles/ts_' + str(vantage_point_index_list[i]) + '.dat'  # get the 20 vp files
-        x = np.loadtxt(file_name, delimiter = ',')  #####
+        x = np.loadtxt(file_name, delimiter=',')  #####
         vantage_point = TimeSeries(x[:, 0], x[:, 1])
         vantage_point_list.append(vantage_point)
 
-    #my_dict = {}
+    # my_dict = {}
 
     for i in range(num_vp):
         dbName = "tsdb/db_" + str(i) + ".dbdb"
@@ -81,7 +83,7 @@ def generateDB(numTS = 1000, numVP = 20):
             os.remove(dbName)
         db = lab10.connect(dbName)
 
-        #my_dict[i] = {}
+        # my_dict[i] = {}
         # create a new directory called tsdb
         for j in range(num_ts):
             tree_name = 'ts_' + str(j)
@@ -90,18 +92,17 @@ def generateDB(numTS = 1000, numVP = 20):
             compare_ts_point = TimeSeries(x[:, 0], x[:, 1])
 
             dist = correlation.kernel_corr_dist(vantage_point_list[i], compare_ts_point)
-            #print(dist, tree_name)
+            # print(dist, tree_name)
             db.set(dist, tree_name)
             db.commit()
-            #my_dict[i][j]=dist
+            # my_dict[i][j]=dist
         db.close()
-    #db.close()
-    #f = open("mytest.dat", "w")
-    #f.write(json.dumps(my_dict))
-    #f.close()
+        # db.close()
+        # f = open("mytest.dat", "w")
+        # f.write(json.dumps(my_dict))
+        # f.close()
 
-
-#if __name__ == "__main__":
+# if __name__ == "__main__":
 #    num_ts = int(sys.argv[1])
 #    num_vp = int(sys.argv[2])
 #    generateDB(num_ts, num_vp)
--- tsbtreedb/similarity.py
+++ tsbtreedb/similarity.py
@@ -1,4 +1,5 @@
 import os, sys
+
 curr_dir = os.getcwd().split('/')
 sys.path.append('/'.join(curr_dir[:-1]))
 ts_dir = curr_dir[:-1]
@@ -26,8 +27,10 @@ tsfiles, tsdb
 Requires files:
 vp_origin_idx.dat
 '''
-def vp_similarity_search(query_ts, vantage_point_index_list):#vantage_point_list should be a file
-#def _vp_similarity_search(query_ts_key, ts_dict, vp_dict):
+
+
+def vp_similarity_search(query_ts, vantage_point_index_list):  # vantage_point_list should be a file
+    # def _vp_similarity_search(query_ts_key, ts_dict, vp_dict):
     '''
     Find a list of 10 nearest time series to the query time series.
     1. get distances from query time series to all vantage points
@@ -48,29 +51,29 @@ def vp_similarity_search(query_ts, vantage_point_index_list):#vantage_point_list
     query_vp_dist = []
     for i in range(len(vantage_point_index_list)):
         file_name = 'tsfiles/ts_' + str(int(vantage_point_index_list[i])) + '.dat'  # get the 20 vp files
-        x = np.loadtxt(file_name, delimiter = ',')  #####
+        x = np.loadtxt(file_name, delimiter=',')  #####
         vantage_point = TimeSeries(x[:, 0], x[:, 1])
         query_vp_dist.append(correlation.kernel_corr_dist(query_ts, vantage_point, 10))
 
     # step 2: define circle radius as 2 times the distance to each vantage point
     radius = [2 * i for i in query_vp_dist]
-    #print (radius)
+    # print (radius)
 
     # step 3: find relative index of time series within each radius distance
     vp_less_dict = {}
     for i in range(20):
         dbName = "tsdb/db_" + str(i) + ".dbdb"
         db = lab10.connect(dbName)
-        #keys, values = db.getAll_LTE(radius)
+        # keys, values = db.getAll_LTE(radius)
         keys = db.find_all_smaller(radius[i])
         vp_less_dict[i] = keys
-    #print('keys',vp_less_dict)
+    # print('keys',vp_less_dict)
 
     # step 4: calculate distance to all time series within the radius
     distance = {}
     for ts_list in list(vp_less_dict.values()):
         for tree_name in ts_list:
-            #print(tree_name)
+            # print(tree_name)
             file_name = 'tsfiles/' + tree_name + '.dat'
             x = np.loadtxt(file_name, delimiter=',')
             compare_ts_point = TimeSeries(x[:, 0], x[:, 1])
@@ -80,20 +83,20 @@ def vp_similarity_search(query_ts, vantage_point_index_list):#vantage_point_list
     # step 5: find 10 closest time series and return the index of the time series
     # find 10 nearest timeseries
     dist_list = list(distance.items())
-    #print(dist_list)
+    # print(dist_list)
     nearest_ts_tuple = sorted(distance.items(), key=lambda dist: dist[1])[:10]
-    #print(nearest_ts_tuple)
-    #nearest_ts_tuple = [(tree_name, dist),...]
+    # print(nearest_ts_tuple)
+    # nearest_ts_tuple = [(tree_name, dist),...]
     return [item[0] for item in nearest_ts_tuple]
 
 
 if __name__ == "__main__":
     file_name = 'vp_origin_idx.dat'
-    vantage_point_index_list = np.loadtxt(file_name, delimiter = ',')
-    #print(vantage_point_index_list)
-    #vantage_point_index_list = [3,33,21,5,11,8,28,13,46,34,23,32,0,49,19,27,37,24,44,2]
-    x = np.loadtxt('tsfiles/ts_1.dat', delimiter = ',')  #####
+    vantage_point_index_list = np.loadtxt(file_name, delimiter=',')
+    # print(vantage_point_index_list)
+    # vantage_point_index_list = [3,33,21,5,11,8,28,13,46,34,23,32,0,49,19,27,37,24,44,2]
+    x = np.loadtxt('tsfiles/ts_1.dat', delimiter=',')  #####
     query_ts = TimeSeries(x[:, 0], x[:, 1])
 
     nearest_ts = vp_similarity_search(query_ts, list(vantage_point_index_list))
-    print('10 nearest time series:',nearest_ts)
+    print('10 nearest time series:', nearest_ts)
