3b40a2d97aaa999098fb1adf5da9aee349b2d8d5
Sophie Hilgard <ashilgard@gmail.com>
2016-12-04 15:53:53 -0500

    db client/socket server

--- timeseries/server.py
+++ timeseries/server.py
@@ -1,33 +1,20 @@
 from socket import AF_INET, SOCK_STREAM, socket, SOL_SOCKET, SO_REUSEADDR
 from concurrent.futures import ThreadPoolExecutor
 import threading
+from .tsdb_ops import *
+from .tsdb_deserialize import *
+from .tsdb_error import *
 import json
 import enum
 
 LENGTH_FIELD_LENGTH = 4
 
-class LockableDict:
-    def __init__(self):
-        self._d={}
-        self._dlock={}
-
-    def __getitem__(self, attr):
-        return self._d[attr]
-
-    def __setitem__(self, attr, val):
-        if attr not in self._d:
-            self._dlock[attr]=threading.Lock()
-        print("LOCKING FOR", attr, val)
-        with self._dlock[attr]:
-            self._d[attr] = val
-        print("UNLOCKED FOR", attr, val)
-
-
 class TSDB_Server(socketserver.BaseServer):
 
-    def __init__(self, db, port=15000):
+    def __init__(self, db, addr=15000):
         self.db = db
-        self.port = port
+        self.addr = port
+        self.deserializer = Deserializer()
 
     def run(self):
         pool = ThreadPoolExecutor(50)
@@ -35,23 +22,39 @@ class TSDB_Server(socketserver.BaseServer):
         sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
         sock.bind(addr)
         sock.listen(15)
-        
+
         while True:
             print('connection')
             client_sock, client_addr = sock.accept()
             pool.submit(handle_client, client_sock, client_addr, self.db)
 
-    def handle_client(sock, client_addr, db):
+    def handle_client(sock, client_addr):
         print('Got connection from', client_addr)
         while True:
             msg = sock.recv(65536)
-            print("msg", msg)
             if not msg:
                 break
-            key, value = msg.decode().split('=')
-            print("k,v", key, value)
-            #change to reflect rbtree instead of ldict
-            ldict[key] = value
-            sock.sendall(value.encode())
+            json_response = data_received(msg)
+            sock.sendall(self.deserializer.serialize(json_response))
         print('Client closed connection')
         sock.close()
+
+    def data_received(self, data):
+        self.deserializer.append(data)
+        if self.deserializer.ready():
+            msg = self.deserializer.deserialize()
+            status = TSDBStatus.OK  # until proven otherwise.
+            response = TSDBOp_Return(status, None)  # until proven otherwise.
+            try:
+                tsdbop = TSDBOp.from_json(msg)
+            except TypeError as e:
+                response = TSDBOp_Return(TSDBStatus.INVALID_OPERATION, None)
+            if status is TSDBStatus.OK:
+                if isinstance(op, TSDBOp_withTS):
+                    response = self._with_ts(tsdbop)
+                elif isinstance(op, TSDBOp_withID):
+                    response = self._with_id(tsdbop)
+                else:
+                    response = TSDBOp_Return(TSDBStatus.UNKNOWN_ERROR, tsdbop['op'])
+
+            return serialize(response.to_json())
