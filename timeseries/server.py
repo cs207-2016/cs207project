73838d8ca685c109dc7c3eb0159b606e25e43786
Sophie Hilgard <ashilgard@gmail.com>
2016-12-04 13:41:24 -0500

    preliminary changes to server class

--- timeseries/server.py
+++ timeseries/server.py
@@ -22,31 +22,36 @@ class LockableDict:
             self._d[attr] = val
         print("UNLOCKED FOR", attr, val)
 
-def db_client(sock, client_addr, ldict):
-    print('Got connection from', client_addr)
-    while True:
-        msg = sock.recv(65536)
-        print("msg", msg)
-        if not msg:
-            break
-        key, value = msg.decode().split('=')
-        print("k,v", key, value)
-        ldict[key] = value
-        sock.sendall(value.encode())
-    print('Client closed connection')
-    sock.close()
 
-def db_server(addr):
-    print("creating lockable dict and pool")
-    ldict=LockableDict()
-    pool = ThreadPoolExecutor(50)
-    sock = socket(AF_INET, SOCK_STREAM)
-    sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    sock.bind(addr)
-    sock.listen(15)
-    while True:
-        print('connection')
-        client_sock, client_addr = sock.accept()
-        pool.submit(db_client, client_sock, client_addr, ldict)
+class TSDB_Server(socketserver.BaseServer):
 
-db_server(('',15000))
+    def __init__(self, db, port=15000):
+        self.db = db
+        self.port = port
+
+    def run(self):
+        pool = ThreadPoolExecutor(50)
+        sock = socket(AF_INET, SOCK_STREAM)
+        sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        sock.bind(addr)
+        sock.listen(15)
+        
+        while True:
+            print('connection')
+            client_sock, client_addr = sock.accept()
+            pool.submit(handle_client, client_sock, client_addr, self.db)
+
+    def handle_client(sock, client_addr, db):
+        print('Got connection from', client_addr)
+        while True:
+            msg = sock.recv(65536)
+            print("msg", msg)
+            if not msg:
+                break
+            key, value = msg.decode().split('=')
+            print("k,v", key, value)
+            #change to reflect rbtree instead of ldict
+            ldict[key] = value
+            sock.sendall(value.encode())
+        print('Client closed connection')
+        sock.close()
