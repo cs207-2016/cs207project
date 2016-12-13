cf6f94fdefbc11e236cd0a2048e1a5092abcf3fc
arielhv <ariel_herbertvoss@g.harvard.edu>
2016-12-13 00:11:55 -0800

    Add timeseries file IO and POST endpoint

--- src/website/website.py
+++ src/website/website.py
@@ -1,13 +1,16 @@
 import logging
 import random
+import string
 from flask import Flask, request, abort, jsonify, make_response
 from flask.ext.sqlalchemy import SQLAlchemy, DeclarativeMeta
-from json import JSONEncoder
+import json
+
+import timeseries
 
 logger = logging.getLogger(__name__)
 
 
-class ProductJSONEncoder(JSONEncoder):
+class ProductJSONEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj.__class__, DeclarativeMeta):
             return obj.to_dict()
@@ -55,65 +58,116 @@ class TimeseriesEntry(db.Model):
 
 def get_floating_range(range_str):
     if "-" not in range_str:
-        raise Exception("Invalid input range: %s" % range_str)
+        raise KeyError("Invalid input range: %s" % range_str)
     range_components = range_str.split('-')
     if len(range_components) != 2:
-        raise Exception("Found too many range indications in: %s" % range_str)
+        raise KeyError("Found too many range indications in: %s" % range_str)
     return float(range_components[0]), float(range_components[1])
 
 @app.route('/timeseries', methods=['GET'])
 def get_all_metadata():
-    """/timeseries endpoint
+    """/timeseries GET endpoint
         Defines two API calls:
             * /timeseries GET - Return a JSON containing metadata info from all timeseries
-            * /timeseries?request=value(s) - Return timeseries that match the request. Valid requests are:
+            * /timeseries?request=value(s) GET - Return timeseries that match the request. Valid requests are:
                 + blarg_in=lower-upper - Get data with a blarg that falls within the floating point range
                 + level_in=X,Y - Get data that contains one of the comma separated letters
                 + mean_in=lower-upper - Get data with a mean that falls within the floating point range
                 + std_in=lower-upper - Get data with a std that falls within the floating point range
     """
-    if 'blarg_in' in request.args:
-        logger.debug("Getting entries by blarg value")
-        lower, upper = get_floating_range(request.args.get('blarg_in'))
-        results = db.session.query(TimeseriesEntry).\
-            filter(TimeseriesEntry.blarg >= lower).\
-            filter(TimeseriesEntry.blarg < upper)
-    elif 'level_in' in request.args:
-        logger.debug("Getting entries by level value")
-        levels = request.args.get('mean_in').split(",")
-        results = db.session.query(TimeseriesEntry).\
-            filter(TimeseriesEntry.level.in_(levels))
-    elif 'mean_in' in request.args:
-        logger.debug("Getting entries by mean value")
-        lower, upper = get_floating_range(request.args.get('mean_in'))
-        results = db.session.query(TimeseriesEntry).\
-            filter(TimeseriesEntry.mean >= lower).\
-            filter(TimeseriesEntry.mean < upper)
-    elif 'std_in' in request.args:
-        logger.debug("Getting entries by std value")
-        lower, upper = get_floating_range(request.args.get('std_in'))
-        results = db.session.query(TimeseriesEntry).\
-            filter(TimeseriesEntry.std >= lower).\
-            filter(TimeseriesEntry.std < upper)
-    else:
-        logger.info("Getting all TimeseriesEntries")
-        results = db.session.query(TimeseriesEntry).all()
-    return jsonify(dict(timeseries=results))
+    try:
+        if 'blarg_in' in request.args:
+            logger.debug("Getting entries by blarg value")
+            lower, upper = get_floating_range(request.args.get('blarg_in'))
+            results = db.session.query(TimeseriesEntry).\
+                filter(TimeseriesEntry.blarg >= lower).\
+                filter(TimeseriesEntry.blarg < upper)
+        elif 'level_in' in request.args:
+            logger.debug("Getting entries by level value")
+            levels = request.args.get('mean_in').split(",")
+            results = db.session.query(TimeseriesEntry).\
+                filter(TimeseriesEntry.level.in_(levels))
+        elif 'mean_in' in request.args:
+            logger.debug("Getting entries by mean value")
+            lower, upper = get_floating_range(request.args.get('mean_in'))
+            results = db.session.query(TimeseriesEntry).\
+                filter(TimeseriesEntry.mean >= lower).\
+                filter(TimeseriesEntry.mean < upper)
+        elif 'std_in' in request.args:
+            logger.debug("Getting entries by std value")
+            lower, upper = get_floating_range(request.args.get('std_in'))
+            results = db.session.query(TimeseriesEntry).\
+                filter(TimeseriesEntry.std >= lower).\
+                filter(TimeseriesEntry.std < upper)
+        else:
+            logger.info("Getting all TimeseriesEntries")
+            results = db.session.query(TimeseriesEntry).all()
+        return jsonify(dict(timeseries=results))
+    except KeyError as e:
+        logger.warning("Invalid timeseries GET request: %s" % str(e))
+        abort(400)
 
+def generate_timeseries_from_json(raw_json):
+    """Return an ArrayTimeSeries object containing the data in a json file with the following format:
+        {
+            time_points = [list of strings]
+            data_points = [list of strings]
+        }
+    """
+    time_points = [float(x) for x in raw_json["time_points"]]
+    data_points = [float(x) for x in raw_json["data_points"]]
+    return timeseries.ArrayTimeSeries(time_points, data_points)
+
+def save_timeseries_to_file(ts):
+    """Saves an ArrayTimeSeries to a local file in the results folder.
+        Each file is given a random name to prevent file conflicts.
+        Note: This system needs to be replaced by the storage manager"""
+    filename = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(20))+".json"
+    time_points, data_points = zip(*ts.iteritems())
+    data = {
+        "time_points" : time_points,
+        "data_points" : data_points
+    }
+    with open(filename, "w") as f:
+        json.dump(data, f)
+    return filename
+
+def load_timeseries_from_file(filename):
+    """Loads an ArrayTimeSeries from a local file"""
+    with open(filename, "r") as f:
+        data = json.load(f)
+    return generate_timeseries_from_json(data)
 
 @app.route('/timeseries', methods=['POST'])
 def create_entry():
-    if not request.json:
+    """/timeseries POST endpoint
+        Defines the following API call:
+            * /timeseries POST - Add a new timeseries to the database with a JSON in the following format:
+                {
+                    time_points = [list of floats]
+                    data_points = [list of floats]
+                } # Note: This may be changed. The ID field has been omitted, as the user should not have that
+    """
+    if not request.json or not "time_points" in request.json or not "data_points" in request.json:
+        logger.warning("Invalid POST request to timeseries")
         abort(400)
-    logger.info('Creating TimeseriesEntry')
+        return
+    logger.debug('Creating TimeseriesEntry')
+    try:
+        ts = generate_timeseries_from_json(request.json) # Create actual timeseries object
+    except Exception as e:
+        logger.warning("Could not create timeseries object with exception: %s" % str(e))
+        abort(400)
+        return
+    mean = ts.mean() # Get mean and standard deviation from object
+    std = ts.std()
     blarg = random.random()
     level = random.choice(["A","B","C","D","E","F"])
-    #save to file and get fpath (replace with storage manager)
-    fpath=""
-    #Create timeseries object from json
-    #get mean and std
-    mean=0
-    std=0
+
+    # Save to file and get fpath (replace with storage manager)
+    fpath = save_timeseries_to_file(ts)
+
+    # Create TimeseriesEntry
     prod = TimeseriesEntry(blarg=blarg, level=level, mean=mean, std=std, fpath=fpath)
     db.session.add(prod)
     db.session.commit()
