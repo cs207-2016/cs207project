87db25db61d2d121564088dcd67a1a8f9ca1676a
Ryan Lapcevic <rlapcevic@g.harvard.edu>
2016-12-13 01:45:33 -0500

    fixed typos

--- init.sh
+++ init.sh
@@ -11,7 +11,7 @@ sudo pip3 install flask Flask_SQLAlchemy easy_install setuptools
 
 # Install our libraries
 make sdist
-sudo pip install ./dist/timeseries*.tar.gz ./dist/rbtree*.tar.gz ./dist/dbserver*.tar.gz
+sudo pip3 install ./dist/timeseries*.tar.gz ./dist/rbtree*.tar.gz ./dist/dbserver*.tar.gz
 
 # Initialize time series database server
 sudo useradd -r -s /bin/false dbserver
